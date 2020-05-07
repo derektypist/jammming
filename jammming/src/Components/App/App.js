310420bd5faf2297743d872ae76b45236d0afcc9
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-07 09:25:36 +0000

    Updated SearchBar.js

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
-import { render } from 'react-dom';
 
-function App() {
+class App extends React.Component {
     render() {
         return (
     <div>
-  <h1>Ja<span class="highlight">mmm</span>ing</h1>
-  <div class="App">
-    <!-- Add a SearchBar component -->
-    <div class="App-playlist">
-      <!-- Add a SearchResults component -->
-      <!-- Add a Playlist component -->
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+    
+    <SearchBar />
+    <div className="App-playlist">
+      <SearchResults />
+      <Playlist />
     </div>
   </div>
 </div>
   );
     }
+        
+    }
   
-}
+
 
 export default App;
