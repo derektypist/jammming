1bde30655c819e2319aecf114bf11c6f7f54b5d5
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-08 09:52:36 +0000

    Added renderAction method at Track.js

---
+++ jammming/src/Components/Track/Track.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Track extends React.Component {
+
+    renderAction() {
+        if (this.props.isRemoval) {
+            return <button className="Track-action">-</button>
+        } else {
+            return <button className="Track-action">+</button>
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div className="Track">
+                <div class="Track-information">
+    <h3><!-- track name will go here --></h3>
+    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+  </div>
+  
+            </div>
+        );
+    }
+    
+}
+
+export default Track;
