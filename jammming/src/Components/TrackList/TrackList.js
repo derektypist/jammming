cebb22617894e42fc83ec39d12dc9af0092219fe
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-08 09:32:04 +0000

    Added export in TrackList.js

--- jammming/src/Components/TrackList/TrackList.js
+++ jammming/src/Components/TrackList/TrackList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Track from './Track';
 
 class TrackList extends React.Component() {
 render() {
     return (
-        <div className="Tracklist">
+        <div className="TrackList">
         </div>
     );
 }
 }
+
+export default TrackList;
