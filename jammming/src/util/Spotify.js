966497422031974b197e8c5540d5e9b581cf079e
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-11 15:18:28 +0000

    Added savePlaylist in Spotify.js

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -42,8 +42,23 @@ const Spotify = {
             album: track.album.name,
             uri: track.uri
 
-        }))
-    })
+        }));
+    });
+    },
+
+    savePlayList(name, trackUris) {
+        if (!name || !trackUris.length) {
+            return;
+        }
+
+        const accessToken = Spotify.getAccessToken();
+        const headers = { Authorization: `Bearer ${accessToken}`};
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', {headers: headers}).then(response => response.json())
+        .then(jsonResponse => {
+            userId = jsonResponse.id;
+        })
     }
 
     
