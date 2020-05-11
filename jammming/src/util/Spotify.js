0236277bc47f80d5db8f7f53bdbf9cbe182d8a11
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-11 13:09:50 +0000

    Added access token in Spotify.js

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -5,7 +5,25 @@ const Spotify = {
         if (accessToken) {
             return accessToken;
         }
+    
+
+    // Check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+        accessToken = accessTokenMatch[1];
+        const expiresIn = Number(expiresInMatch[1]);
+        window.setTimeout(() => accessToken = '', expiresIn * 1000);
+        window.history.pushState('Access Token', null, '/');
+        return accessToken;
     }
-};
+    
+}
+
+    
+
+
+
+}
 
 export default Spotify;
